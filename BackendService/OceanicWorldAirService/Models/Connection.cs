377b9c70a2cdc436149fd7930adfb78a627a503d
Adam Oliv <adol@netcompany.com>
2022-11-30 10:56:29 +0100

    added node models

---
+++ BackendService/OceanicWorldAirService/Models/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanicWorldAirService.Models
+{
+    public class Connection
+    {
+        public Node startNode;
+        public Node ConnectedNode;
+        public int transportationMethod;
+        public int Cost()
+        {
+
+            int cost = 0;
+            if (this.transportationMethod == 0) //plane
+            {
+                //call caluclate plane cost
+            }
+            else if (this.transportationMethod == 0) //car
+            {
+                //call calculate car api
+            }
+            else if (this.transportationMethod == 0) //boat
+            {
+                //call calculate boat api
+            }
+
+            return cost;
+        }
+    }
+}
---
+++ BackendService/OceanicWorldAirService/Models/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OceanicWorldAirService.Models
+{
+    public class Node
+    {
+
+        public string name;
+        public List<Connection> Connections;
+        public int? MinCostToStart;
+        public Node NearestToStart;
+        public bool Visited;
+
+    }
+
+}
+
