4caa9ff1e348ce21ae0e2e84c11ee02f87874090
Jakub Dobrowolski <jado@netcompany.com>
2022-12-01 13:49:17 +0100

    Deploy

---
+++ BackendService/OceanicWorldAirService/Context/DatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OceanicWorldAirService.Models;
+using RouteModel = OceanicWorldAirService.Models.Route;
+
+namespace OceanicWorldAirService.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        /// <summary>
+        /// DbSet for <see cref="Booking"/>.
+        /// </summary>
+        public DbSet<Booking> Bookings { get; set; } = null!;
+
+        /// <summary>
+        /// DbSet for <see cref="Connection"/>.
+        /// </summary>
+        public DbSet<Connection> Connections { get; set; } = null!;
+
+        /// <summary>
+        /// DbSet for <see cref="Costs"/>.
+        /// </summary>
+        public DbSet<Costs> Costs { get; set; } = null!;
+
+        /// <summary>
+        /// DbSet for <see cref="Customer"/>.
+        /// </summary>
+        public DbSet<Customer> Customers { get; set; } = null!;
+
+        /// <summary>
+        /// DbSet for <see cref="Node"/>.
+        /// </summary>
+        public DbSet<Node> Nodes { get; set; } = null!;
+
+        /// <summary>
+        /// DbSet for <see cref="Parcel"/>.
+        /// </summary>
+        public DbSet<Parcel> Parcels { get; set; } = null!;
+
+        /// <summary>
+        /// DbSet for <see cref="RouteModel"/>.
+        /// </summary>
+        public DbSet<RouteModel> Routes { get; set; } = null!;
+    }
+}
--- BackendService/OceanicWorldAirService/Program.cs
+++ BackendService/OceanicWorldAirService/Program.cs
@@ -1,6 +1,7 @@
 using OceanicWorldAirService.Helpers;
 using OceanicWorldAirService.Services;
 using Microsoft.EntityFrameworkCore;
+using OceanicWorldAirService.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,19 +11,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "OceanicAir",
+        Version = "v1"
+    });
+});
 
 builder.Services.AddScoped<IRouteFindingService, RouteFindingService>();
-
-RegisterDbContext(builder);
+builder.Services.AddDbContext<DatabaseContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+});
 
 app.UseHttpsRedirection();
 
@@ -31,22 +40,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-static void RegisterDbContext(WebApplicationBuilder builder)
-{
-    // Database ConnectionString Options
-    builder.Services.Configure<ConnectionStringOptions>(builder.Configuration.GetSection(ConnectionStringOptions.ConnectionStrings));
-    var connectionStringOptions = new ConnectionStringOptions();
-    builder.Configuration.GetSection(ConnectionStringOptions.ConnectionStrings).Bind(connectionStringOptions);
-
-    // To be implemented - Db connection
-    //
-    //var connectionString = connectionStringOptions.ServiceDatabase;
-    //var serverVersion = ServerVersion.AutoDetect(connectionString);
-    //builder.Services.AddDbContext<FinanceServiceContext>(
-    //    dbContextOptions => dbContextOptions
-    //        .UseMySql(connectionString, serverVersion)
-    //        .LogTo(Console.WriteLine, LogLevel.Information) // The following three options help with debugging
-    //        .EnableSensitiveDataLogging()
-    //        .EnableDetailedErrors());
-}
