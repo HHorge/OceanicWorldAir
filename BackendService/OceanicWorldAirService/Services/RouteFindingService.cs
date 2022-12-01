e57303a2e5d282b035e6a61be8bcbf27c54660a3
Adam Oliv <adol@netcompany.com>
2022-12-01 09:12:52 +0100

    added basic djikstra

--- BackendService/OceanicWorldAirService/Services/RouteFindingService.cs
+++ BackendService/OceanicWorldAirService/Services/RouteFindingService.cs
@@ -18,5 +18,55 @@
 
             throw new NotImplementedException();
         }
+        public List<Node> GetShortestPathDijkstra(Node start, Node end)
+        {
+            DijkstraSearch(start, end);
+            var shortestPath = new List<Node>();
+            shortestPath.Add(end);
+            BuildShortestPath(shortestPath, end);
+            shortestPath.Reverse();
+            return shortestPath;
+        }
+
+        public void BuildShortestPath(List<Node> list, Node node)
+        {
+            if (node.NearestToStart == null)
+                return;
+            list.Add(node.NearestToStart);
+            BuildShortestPath(list, node.NearestToStart);
+        }
+
+        private void DijkstraSearch(Node start, Node end)
+        {
+            start.MinCostToStart = 0;
+            var prioQueue = new List<Node>();
+            prioQueue.Add(start);
+            do
+            {
+                prioQueue = prioQueue.OrderBy(x => x.MinCostToStart).ToList();
+                var node = prioQueue.First();
+                prioQueue.Remove(node);
+                foreach (var cnn in node.Connections)
+                {
+                    var childNode = cnn.ConnectedNode;
+                    if (childNode.Visited)
+                        continue;
+                    if (childNode.MinCostToStart == null ||
+                        node.MinCostToStart + cnn.Cost() < childNode.MinCostToStart)
+                    {
+                        childNode.MinCostToStart = node.MinCostToStart + cnn.Cost();
+                        childNode.NearestToStart = node;
+                        if (!prioQueue.Contains(childNode))
+                            prioQueue.Add(childNode);
+                    }
+                }
+                node.Visited = true;
+                if (node == end)
+                    return;
+            } while (prioQueue.Any());
+        }
+
+
+
     }
 }
