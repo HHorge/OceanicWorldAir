1283ec68c9e75aa49bf814313787cf4aa63d42ed
Morten Trier Bach <motb@netcompany.com>
2022-12-01 10:00:01 +0100

    GUID on Parcels for integration

---
+++ BackendService/OceanicWorldAirService/Services/IRouteFindingService - Copy.cs
@@ -0,0 +1,16 @@
+﻿using OceanicWorldAirService.Models;
+using RouteModel = OceanicWorldAirService.Models.Route;
+
+namespace OceanicWorldAirService.Services
+{
+    /// <summary>
+    /// RouteFindingService interface.
+    /// </summary>
+    public interface IRouteFindingService
+    {
+        /// <summary>
+        /// Method for retrieving the calculated Routes.
+        /// </summary>
+        public Task<IEnumerable<RouteModel>> FindRoutes(List<Parcel> parcelList, string startCity, string destinationCity);
+    }
+}
